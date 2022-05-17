701ef06b2f86df0ebe49176e55eabc496fee3efa
Paolo Antonio Rossi <paoloantoniorossi@gmail.com>
2022-05-17 18:30:38 +0200

    restore unstoppable.challenge.js

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -39,7 +39,6 @@ describe('[Challenge] Unstoppable', function () {
 
   it('Exploit', async function () {
     /** CODE YOUR EXPLOIT HERE */
-    await this.token.transfer(this.pool.address, 10);
   });
 
   after(async function () {
